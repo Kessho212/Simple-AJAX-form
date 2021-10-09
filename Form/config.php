d5860d3364c60be73ba39943e71c8f9bc3186699
Kessho212 <pkonopko@DESKTOP-JM8IALE.localdomain>
2021-10-09 09:05:45 +0200

    zmiana pliku configuracyjnego

--- Form/config.php
+++ Form/config.php
@@ -1,9 +1,9 @@
 <?php
 include_once("mysql.class.php");
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "zadanie";
+$servername = "db_host";
+$username = "db_username";
+$password = "db_passowrd";
+$dbname = "db_name";
 
 $MySQLiObject = new MySQLiObject;
 $MySQLiObject -> connect($servername, $username, $password, $dbname);
